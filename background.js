b8de3686942134f0973a58bc61fa28d3729cb162
Christopher <chrisatenorio@gmail.com>
2023-06-25 10:28:19 -0700

    access current tab url

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
 import equitableCheck from "../equitableCheck.json";
 
-chrome.action.onClicked.addListener(async (tab) => {});
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.tabs.query({ active: true }, function (tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+  });
+});
