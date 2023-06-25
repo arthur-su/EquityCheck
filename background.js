9d8e6b15ba2cb19f64cd9f583c748cd1178560dd
Christopher <chrisatenorio@gmail.com>
2023-06-25 11:24:37 -0700

    attempt to compare url and company names

--- background.js
+++ background.js
@@ -1,8 +1,22 @@
-import equitableCheck from "../equitableCheck.json";
+const proCompanies = require("../equitableCheck.json");
 
 chrome.action.onClicked.addListener(async (tab) => {
-  chrome.tabs.query({ active: true }, function (tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-  });
+  // Get the current tab URL
+  const url = window.location.href;
+
+  // Compare the URL to the JSON object
+  const found = false;
+  for (const companyName of proCompanies) {
+    if (url.indexOf(companyName) > -1) {
+      found = true;
+      break;
+    }
+  }
+
+  // If the text is found in the JSON object, print a message
+  if (found) {
+    console.log("The text was found in the JSON object.");
+  } else {
+    console.log("The text was not found in the JSON object.");
+  }
 });
