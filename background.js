666b7fba4bde362f3c92638ccbe64a7826724ad3
Christopher <chrisatenorio@gmail.com>
2023-06-25 12:35:37 -0700

    unsure of error

--- background.js
+++ background.js
@@ -4,6 +4,10 @@ chrome.action.onClicked.addListener(async (tab) => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var currentUrl = tabs[0].url;
     console.log("Current URL: " + currentUrl);
+
+    for (currentUrl in proCompanies){
+      console.log("found")
+    }
   });
 }
 // chrome.commands.onCommand.addListener((command) => {
