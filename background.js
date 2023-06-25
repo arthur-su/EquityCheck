fe59b50c4c2cb3f36da21b2e35edbd434bf3e2a8
Christopher <chrisatenorio@gmail.com>
2023-06-25 12:28:40 -0700

    can't get url

--- background.js
+++ background.js
@@ -1,22 +1,11 @@
 const proCompanies = require("../equitableCheck.json");
 
 chrome.action.onClicked.addListener(async (tab) => {
-  // Get the current tab URL
-  const url = window.location.href;
-
-  // Compare the URL to the JSON object
-  const found = false;
-  for (const companyName of proCompanies) {
-    if (url.indexOf(companyName) > -1) {
-      found = true;
-      break;
-    }
-  }
-
-  // If the text is found in the JSON object, print a message
-  if (found) {
-    console.log("The text was found in the JSON object.");
-  } else {
-    console.log("The text was not found in the JSON object.");
-  }
-});
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var currentUrl = tabs[0].url;
+    console.log("Current URL: " + currentUrl);
+  });
+}
+// chrome.commands.onCommand.addListener((command) => {
+//   console.log(`Command: ${command}`);
+// });
